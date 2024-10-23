76168e0e078d457ba07068be3ae6e33c7ba6c225
Viacheslav-ua <atph@ukr.net>
2024-10-23 09:45:46 +0300

    enabled, isLoading, isPending, isFetching

--- src/modules/todo-list/TodoList.tsx
+++ src/modules/todo-list/TodoList.tsx
@@ -5,20 +5,27 @@ import { useState } from "react"
 export const TodoList = () => {
 
   const [page, setPage] = useState(1)
+  const[enabled, setEnabled] = useState(false)
 
-const {data: todoPage, error, isPending, isFetching, isPlaceholderData } = useQuery({
+const {data: todoPage, error, isPending, isFetching, isLoading,
+  status, fetchStatus, isPlaceholderData } = useQuery({
   queryKey: ['tasks', 'list', {page}],
   queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
   placeholderData: keepPreviousData,
+  enabled,
 })
 
-if (isPending) return (<div className="text-3xl font-bold text-green-700">Loading...</div>)
+console.log(status, fetchStatus);
+
+
+// if (status === 'pending' && fetchStatus === 'fetching') return (<div className="text-3xl font-bold text-green-700">Loading...</div>)
+if (isLoading) return (<div className="text-3xl font-bold text-green-700">Loading...</div>)
 if (error) return <div>Error: {JSON.stringify(error)}</div>
 
   return (
     <div className={"p-5 mp-10 mx-auto max-w-[1200px]" + (isPlaceholderData ? " text-red-600" : "")}>
       <h1 className="text-3xl underline mb-5 capitalize">To do list</h1>
-
+      <button onClick={() => setEnabled(e => !e)}>Toggle enabled</button>
       <div className="flex flex-col gap-4">
         {todoPage?.data.map((task) => (
           <div 
@@ -36,7 +43,7 @@ if (error) return <div>Error: {JSON.stringify(error)}</div>
 
       <button 
         className="p-2 border rounded border-teal-500"
-        onClick={() => setPage(p => Math.min(todoPage.pages, p + 1))}
+        onClick={() => setPage(p => Math.min(todoPage?.pages ?? 1, p + 1))}
       >next</button>
 
       <button 
@@ -46,7 +53,7 @@ if (error) return <div>Error: {JSON.stringify(error)}</div>
         
       <button 
         className="p-2 border rounded border-teal-500"
-        onClick={() => setPage(todoPage.last)}
+        onClick={() => setPage(todoPage?.last ?? 1)}
       >last</button>
 
       <p>Page: {page}</p>
